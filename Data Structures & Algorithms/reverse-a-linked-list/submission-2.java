6c2930415fc5d4198163444b1b60377239473d8b
Emanuel Lisandro <102778540+EmanuelSLisandro@users.noreply.github.com>
2026-06-10 11:59:08 -0300

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       ListNode  atual = head;
+       ListNode  anterior = null;
+        while(atual != null){
+            ListNode proximo = atual.next;
+            atual.next = anterior;
+            anterior = atual;
+            atual = proximo;
+        }
+        return anterior;
+    }
+}
