48ae47671345ca489fa12fbbb89be9f96829cc31
Emanuel Lisandro <102778540+EmanuelSLisandro@users.noreply.github.com>
2026-06-12 14:00:42 -0300

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    boolean isPalin(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            } else {
+                return isPalin(s, left+1, right) || isPalin(s, left, right-1);
+            }
+        }
+        return true;
+    }
+}
